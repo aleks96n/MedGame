1806951eb2800bbc8917dd95403159d81c32b204
aleks96n <aleksnik1996@gmail.com>
2018-02-26 14:20:33 +0200

    First game of many

---
+++ Assets/Scripts/ClickOnActor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickOnActor : MonoBehaviour {
+
+    public Transform target;
+    private Animation anim;
+    private bool clickedON = false;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (clickedON)
+        {
+            var speed = 5;
+            //Debug.Log("I got clicked");
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            anim.Play("moveAnimation");
+        }
+    }
+
+    void OnMouseDown()
+    {
+        clickedON = true;
+    }
+}
---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+
+    private Animation animation;
+	// Use this for initialization
+	void Start () {
+        animation = gameObject.GetComponent<Animation>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision coll)
+    {
+        Debug.Log("touched");
+        animation.Play("DoorOpen");
+
+    }
+}
